b2fbaa6baa2d0e4db438840f98101f8ae861a7e1
amit <amit.kumar@gslab.com>
2016-11-04 11:39:48 +0530

    small and minor updation for beginning of exercise

--- decisive.py
+++ decisive.py
@@ -1,3 +1,18 @@
 
 
-import pygame
+import pygame,sys      # needeed to import pyfgame and sys module
+from pygame.locals import *  #  contains several constant variables that are easy to identify as being in the pygame.locals module without pygame.locals. i
+
+# Surface objects refer to 2-D immages
+pygame.init()   #initialising pygame module
+FPS= 30
+fpsClock = pygame.time.Clock()
+DISPLAYSURF = pygame.display.set_mode((400, 300)) # returns the pygame.Surface object for window.This functions tell (width,height) of window in pixels
+pygame.display.set_caption('Game!')#making text appears as caption of the window
+
+White = (255,255,255)
+
+
+
+
+
